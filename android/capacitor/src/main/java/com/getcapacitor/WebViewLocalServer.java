5d43274ffb5d7da5320e8fe399a87997fd287ac8
macdja38 <jakeincanada@icloud.com>
2022-05-18 12:05:28 -0400

    Allow loading from localhost even when a server url is configured

--- android/capacitor/src/main/java/com/getcapacitor/WebViewLocalServer.java
+++ android/capacitor/src/main/java/com/getcapacitor/WebViewLocalServer.java
@@ -208,6 +208,9 @@ public class WebViewLocalServer {
     }
 
     private boolean isMainUrl(Uri loadingUrl) {
+        if (loadingUrl.getHost().compareToIgnoreCase("localhost") == 0) {
+            return true;
+        }
         return (bridge.getServerUrl() == null && loadingUrl.getHost().equalsIgnoreCase(bridge.getHost()));
     }
 
