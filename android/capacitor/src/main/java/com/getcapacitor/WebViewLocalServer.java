a40a0884da1462fde77c149fdae2c86fd73fbd11
macdja38 <jakeincanada@icloud.com>
2023-07-25 16:05:03 -0400

    Remove duplicated cookie sending

--- android/capacitor/src/main/java/com/getcapacitor/WebViewLocalServer.java
+++ android/capacitor/src/main/java/com/getcapacitor/WebViewLocalServer.java
@@ -484,10 +484,6 @@ public class WebViewLocalServer {
                         for (Map.Entry<String, String> header : headers.entrySet()) {
                             conn.setRequestProperty(header.getKey(), header.getValue());
                         }
-                        String getCookie = CookieManager.getInstance().getCookie(url);
-                        if (getCookie != null) {
-                            conn.setRequestProperty("Cookie", getCookie);
-                        }
                         conn.setRequestMethod(method);
                         conn.setReadTimeout(30 * 1000);
                         conn.setConnectTimeout(30 * 1000);
